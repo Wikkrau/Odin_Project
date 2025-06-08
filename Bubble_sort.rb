3a9c72a2e11d0af039741464532617df906e9262
Wikkrau <wikkrau@gmail.com>
2025-06-08 21:09:36 +0200

    Bubble_sort system

---
+++ Bubble_sort.rb
@@ -0,0 +1,16 @@
+def bubble_sort(arr)
+  n = arr.length
+  loop do 
+        swapped = false
+        (n-1).times do |i|
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i] # swap
+                swapped = true
+            end
+        end
+        break unless swapped
+        
+  end
+  return arr
+end
+puts bubble_sort([4,3,78,2,0,2])
