a5782f9a8a24d344132b9e8211f9a12eb3adfef0
Wikkrau <wikkrau@gmail.com>
2025-06-08 17:35:07 +0200

    New version of Caesar_cipher

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,15 +1,19 @@
 $vocabulary = "abcdefghijklmnopqrstuvwxyz".split("")
+$vocabulary_big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 
 def caesar_cipher(word, shift)
   shift = shift.to_i % 26
-  word.downcase.chars.map do |letter|
+  word.chars.map do |letter|
     if $vocabulary.include?(letter)
       index = $vocabulary.find_index(letter)
       $vocabulary[(index + shift) % 26]
+    elsif $vocabulary_big.include?(letter)
+      index = $vocabulary_big.find_index(letter)
+      $vocabulary_big[(index + shift) % 26]
     else
       letter
     end
-  end.join("").capitalize
+  end.join("")
 end
 
 puts caesar_cipher("What a string!", 5)
