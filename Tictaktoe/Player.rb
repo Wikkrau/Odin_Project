aa8d1ab38dff81d5b6c1130d84b495b95d0df4a8
Wikkrau <wikkrau@gmail.com>
2025-06-17 21:27:26 +0200

    Redo Tiktaktoe

---
+++ Tictaktoe/Player.rb
@@ -0,0 +1,12 @@
+class Player
+  attr_reader :name, :tag
+
+  def initialize(name, tag)
+    @name = name.capitalize
+    @tag = tag  
+  end
+
+  def name_display
+    "#{@name} is a: #{@tag}"
+  end
+end
